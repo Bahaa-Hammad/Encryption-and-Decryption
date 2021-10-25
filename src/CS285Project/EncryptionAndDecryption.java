583b13dc745e026925b4b37e9031a944a4109ed7
Bahaa <219110085@psu.edu.sa>
2021-10-25 19:15:19 +0300

    Full Refactoring, changed chars to Strings and fixed lines problems.
    Next Modification: Refactoring:
                       Changing methods to classes
                       putting prime numbers as input to the user
                       Adding comments
                       Technical Report

--- src/CS285Project/EncryptionAndDecryption.java
+++ src/CS285Project/EncryptionAndDecryption.java
@@ -1,42 +1,83 @@
 package CS285Project;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+
+import java.util.Scanner;
 
 public class EncryptionAndDecryption {
 
-    public static void main(String[] args) {
-        ArrayList<Character> arr = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner scan = new Scanner(file);
+        String line = "";
+        String encryptLine;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-            String line;
-            while((line = reader.readLine()) != null) {
-                for (int i = 0; i < line.length(); i++) {
-                    char c = line.charAt(i);
-                    arr.add(getAscii(encrypt(c)));
-                }
-                arr.add(' ');
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+
+            while (scan.hasNextLine()) {
+                encryptLine = encrypt(line.concat(""+ scan.nextLine()));
+                writer.write(decrypt(encryptLine));
+                writer.newLine();
             }
-            reader.close();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(arr.toString());
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("encrypted.txt"));
+            scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                encryptLine = encrypt(line.concat(""+ scan.nextLine()));
+                writer.write(encryptLine);
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public static int encrypt(char c){
+    public static String encrypt(String line){
         int p1 = 7;
         int p2 = 3;
-        int index = getAsciiIndex(c);
-        int encryptionIndex = (p1 * index + p2) % 256;
-        return encryptionIndex;
+        char[] arr = line.toCharArray();
+        char[] encryptedLine = new char[line.length()];
+        for(int i = 0; i < arr.length; i++){
+            int index = getAsciiIndex(arr[i]);
+            int encryptionIndex = ((p1) * (index) + (p2)) % 256;
+            encryptedLine[i] = getAscii(encryptionIndex);
+        }
+        return concat(encryptedLine);
+    }
+
+
+    public static String decrypt(String line){
+        char[] arr = line.toCharArray();
+        char[] decryptionLine = new char[line.length()];
+        int alpha;
+        int p1 = 7, p2 = 3;
+        int result;
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int  k = getAsciiIndex(arr[i]);
+            result = -1;
+            alpha = -1;
+            while (result != 0){
+                alpha++;
+                result = (k - (p1 * alpha + p2)) % 256;
+            }
+
+            decryptionLine[i] = getAscii((alpha));
+        }
+        return concat(decryptionLine);
     }
 
 
     public static int getAsciiIndex(char c){
-        int a = (int)c;
+        int a = c;
         return a;
     }
 
@@ -44,5 +85,17 @@ public class EncryptionAndDecryption {
     public static char getAscii(int index){
         char x = (char) index;
         return x;
+    }
+
+
+    public static String concat(char[] chars) {
+        if (chars.length == 0) {
+            return "";
+        }
+        StringBuilder s = new StringBuilder(chars.length);
+        for (char c : chars) {
+            s.append(c);
+        }
+        return s.toString();
     }
 }
