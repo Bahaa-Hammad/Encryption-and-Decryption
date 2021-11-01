029de935f10bf3469a03473ace82ae5f322efcff
bahaa <219110085@psu.edu.sa>
2021-11-02 00:51:25 +0300

    Total Restructure ;(

--- src/CS285Project/EncryptionAndDecryption.java
+++ src/CS285Project/EncryptionAndDecryption.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class EncryptionAndDecryption {
 
     public static void main(String[] args) throws FileNotFoundException {
-        //User Prime Number input:
+        ///User Prime Number input:
         Scanner pInput = new Scanner(System.in);
         System.out.println("Enter The First Prime Number: ");
         int p1 = pInput.nextInt();
@@ -16,94 +16,98 @@ public class EncryptionAndDecryption {
 
 
 
+        /*
+            Encrypting a File:
+        */
         File file = new File("input.txt");
         Scanner scan = new Scanner(file);
-        String line = "";
-        String encryptedLine;
-        // The Encrypted File:
+        String input = "";
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("encrypted.txt"));
-            while (scan.hasNextLine()) { // Take one line at a time.
-                encryptedLine = encrypt(scan.nextLine(),p1,p2);
-                writer.write(encryptedLine);
-                writer.newLine();
+            // Reading
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                input= input.concat(line) + "\r"; // "\r" -> Marks a new line in the string.
             }
+            scan.close();
+            input = input.substring(0, input.length() - 1);
+            writer.write(encrypt(input, p1, p2));
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Decrypting The Encrypted file
+
+        /*
+            Decrypting The Encrypted file:
+        */
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
             file = new File("encrypted.txt");
             scan = new Scanner(file);
-
+            String out = "";
             while (scan.hasNextLine()) { // Take one line at a time.
-                encryptedLine = scan.nextLine(); // Encrypted
-                writer.write(decrypt(encryptedLine, p1, p2));
-                writer.newLine();
+                String line = scan.nextLine();
+                out = out.concat(line) + "\r";
             }
+            scan.close();
+            out = out.substring(0, out.length()-1);
+            writer.write(decrypt(out, p1, p2));
             writer.close();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public static String encrypt(String line, int p1, int p2){
-        char[] arr = line.toCharArray();
-        char[] encryptedLine = new char[line.length()];
-        for(int i = 0; i < arr.length; i++){
-            int index = getAsciiIndex(arr[i]);
+    public static String encrypt(String line, int p1, int p2) {
+        String encrypted = "";
+        for (int i = 0; i < line.length(); i++) {
+            int index = getAsciiIndex(line.charAt(i));
             int encryptionIndex = ((p1) * (index) + (p2)) % 128;
-            encryptedLine[i] = getAscii(encryptionIndex);
+            encrypted = encrypted.concat(append(getAscii((encryptionIndex))));
         }
-        return concat(encryptedLine);
+        return encrypted;
     }
 
 
-    public static String decrypt(String line, int p1, int p2){
-        char[] arr = line.toCharArray();
-        char[] decryptionLine = new char[line.length()];
+    public static String decrypt(String line, int p1, int p2) {
+        String decrypted = "";
         int alpha;
         int result;
 
-        for (int i = 0; i < arr.length; i++) {
-            int  k = getAsciiIndex(arr[i]);
+
+        for (int i = 0; i < line.length(); i++) {
+            int k = getAsciiIndex(line.charAt(i));
             result = -1;
             alpha = -1;
-            while (result != 0){
+            while (result != 0) {
                 alpha++;
                 result = (k - (p1 * alpha + p2)) % 128;
             }
 
-            decryptionLine[i] = getAscii((alpha));
-            System.out.println(alpha);
+            decrypted = decrypted.concat(append(getAscii((alpha))));
         }
-        return concat(decryptionLine);
+        return decrypted;
     }
 
 
-    public static int getAsciiIndex(char c){
+    public static int getAsciiIndex(char c) {
         return c;
     }
 
 
-    public static char getAscii(int index){
+    public static char getAscii(int index) {
         return (char) index;
     }
 
 
-    public static String concat(char[] chars) {
-        if (chars.length == 0) {
-            return "";
-        }
-        StringBuilder s = new StringBuilder(chars.length);
-        for (char c : chars) {
-            s.append(c);
-        }
+    public static String append(char chars) {
+        StringBuilder s = new StringBuilder(1);
+        s.append(chars);
         return s.toString();
     }
 }
